3f35d8fe435515667eac9e5e2f10560874fe6ff3
mike spindel <mike@spindel.is>
2015-07-02 11:53:43 -0700

    Fix GLSL merge

--- pysrc/pcbre/resources/shaders/image_frag.glsl
+++ pysrc/pcbre/resources/shaders/image_frag.glsl
@@ -5,11 +5,8 @@ uniform sampler2D tex1;
 in vec2 pos;
 out vec4 FragColor;
 
-out vec4 FragColor;
-
 void main(void)
 {
     vec4 tex = texture(tex1, pos);
     FragColor = vec4(tex.r, tex.g, tex.b, 1);
 }
-
