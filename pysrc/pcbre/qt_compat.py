a2e9e2b6d68b2ab31835cb6b6d5c142c516cc85e
mike spindel <mike@spindel.is>
2016-10-14 19:32:59 -0700

    Remove non-PyQt5 compat

--- pysrc/pcbre/qt_compat.py
+++ pysrc/pcbre/qt_compat.py
@@ -3,48 +3,10 @@ import os
 default_binding = "pyqt5"
 
 requested_binding = os.environ.get('QT_API', '').lower()
-if requested_binding not in ("pyqt4", "pyside", "pyqt5"):
+if requested_binding not in ("pyqt5"):
     requested_binding = default_binding
 
-if requested_binding == "pyqt4":
-    from PyQt4 import QtGui, QtCore, QtOpenGL
-
-    QtCore.Signal = QtCore.pyqtSignal
-    QtCore.Slot = QtCore.pyqtSlot
-
-    def QGLContext(f):
-        return QtOpenGL.QGLContext(f, None)
-
-    def QtLoadUI(f):
-        from pcbre.qt_compat import QtUiTools
-        loader = QtUiTools.QUiLoader()
-        return loader.load(f)
-
-    def getOpenFileName(*args, **kwargs):
-        return QtGui.QFileDialog.getOpenFileName(*args, **kwargs)
-
-    def getSaveFileName(*args, **kwargs):
-        return QtGui.QFileDialog.getSaveFileName(*args, **kwargs)
-
-elif requested_binding == "pyside":
-    from PySide import QtGui, QtCore, QtOpenGL
-
-    def QGLContext(f):
-        return QtOpenGL.QGLContext(f)
-
-    def QtLoadUI(f):
-        from PySide import uic
-        return uic.loadUi(f)
-
-    def getOpenFileName(*args):
-        fn, _ = QtGui.QFileDialog.getOpenFileName(*args)
-        return fn
-
-    def getSaveFileName(*args, **kwargs):
-        fn, _ = QtGui.QFileDialog.getSaveFileName(*args, **kwargs)
-        return fn
-
-elif requested_binding == "pyqt5":
+if requested_binding == "pyqt5":
     from PyQt5 import QtGui, QtCore, QtOpenGL, QtWidgets
 
     QtCore.Signal = QtCore.pyqtSignal
